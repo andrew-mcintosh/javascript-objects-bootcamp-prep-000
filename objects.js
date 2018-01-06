b511b6b0321324434838b4bb5658c857d96102e5
Andrew McIntosh <andrewdavidmcintosh@gmail.com>
2018-01-06 20:39:06 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 function removeFromPlaylist(playlist, artistName, songTitle) {
   delete playlist.artistName;
-return Object.assign({}, playlist, {[artistName]: songTitle});
+return Object.assign({}, playlist, {artistName: songTitle});
 }
 
 function updatePlaylist(playlist, artistName, songTitle) {
